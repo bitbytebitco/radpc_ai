31ffe31b2bd554d25efae1cb2a32b96f998c0030
Zachary Becker <bitbytebitco@gmail.com>
2021-05-12 15:31:19 -0600

    Working SPI interface where packet is being sent (Thanks to help from Colter).

--- spi_test.py
+++ spi_test.py
@@ -7,25 +7,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23,GPIO.IN)
 
 # SPI init
-spi_bus = 0
+spi_bus =0 
 spi_device = 0
 
 spi = spidev.SpiDev()
 spi.open(spi_bus, spi_device)
-spi.max_speed_hz = 1000000
+spi.max_speed_hz = 100000
 
 def send_receive():
     try:
         # Send a null byte to check for value
-        send_byte = 0x80
-        rcv_byte = spi.xfer2([send_byte])
+        #send_byte = [0x00]
+        send_byte = [x for x in range(0,128)] 
 
         # repeat to check for a response
-        rcv_byte = spi.xfer2([send_byte])
-        data_recv = rcv_byte[0]
+        #rcv_byte = spi.xfer2(send_byte)
+        #data_recv = rcv_byte[0:127]
+        data_recv = spi.xfer3(send_byte,128)
 
-        if (data_recv != 0x80):
-            print ("Response: "+str(data_recv))
+        #if (data_recv != 0x80):
+        print ("Response: "+str(data_recv))
     except Exception as e:
         print(e)
 
@@ -35,6 +36,6 @@ while True:
         send_receive() 
     else:
         print('LOW')
-    time.sleep(0.5)
+    time.sleep(0.15)
 
 #quit()
