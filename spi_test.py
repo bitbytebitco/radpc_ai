2f31744208a734cb60951655b7954e5e856fe8fe
Zachary Becker <bitbytebitco@gmail.com>
2021-05-13 16:13:53 -0600

    Adding SPI code changes for receiving different commands (reset, receive packet).

--- spi_test.py
+++ spi_test.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 # GPIO init
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23,GPIO.IN)
+GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # SPI init
 spi_bus =0 
@@ -14,28 +14,43 @@ spi = spidev.SpiDev()
 spi.open(spi_bus, spi_device)
 spi.max_speed_hz = 100000
 
-def send_receive():
+def send_receive(channel):
     try:
         # Send a null byte to check for value
         #send_byte = [0x00]
+        '''
         send_byte = [x for x in range(0,128)] 
-
-        # repeat to check for a response
-        #rcv_byte = spi.xfer2(send_byte)
-        #data_recv = rcv_byte[0:127]
         data_recv = spi.xfer3(send_byte,128)
-
-        #if (data_recv != 0x80):
-        print ("Response: "+str(data_recv))
+        '''
+        command = spi.xfer2([0x00])[0]
+        print(command)
+        if hex(command) == "0x22":
+            print('Command: RESET')
+            spi.xfer([command])
+        elif hex(command) == "0x25": 
+            print('packet command') 
+            res = spi.xfer([command])
+            print(res)
+            send_byte = [x for x in range(0,128)] 
+            data_recv = spi.xfer3(send_byte,128)
+            print(data_recv)
+            print("".join([chr(x) for x in data_recv]))
+        else:
+            print ("Response: "+str(hex(data_recv[0])))
     except Exception as e:
         print(e)
 
+GPIO.add_event_detect(23, GPIO.RISING, callback=send_receive, bouncetime=500)
+
+while True:
+    pass 
+'''
 while True:
     if GPIO.input(23):
         print('HIGH')
-        send_receive() 
+        send_receive()
     else:
         print('LOW')
     time.sleep(0.15)
-
+'''
 #quit()
